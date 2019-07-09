a647f0e24e8c3a343970f36a6b88ca4f3b02aa19
Alexei Mironov <fariou1994@gmail.com>
2019-07-10 00:29:33 +0400

    exercise 2. calculator added

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-alert('Hello Mediasoft!');
+'use strict';
+
+class Calculator {
+    evaluateExpression(rawExpression) {
+        try {
+            let evaluatedValue = eval(rawExpression);
+            if (evaluatedValue) {
+                return evaluatedValue;
+            }
+        } catch { }
+        throw new Error("incorrect expression");
+    }
+}
+
+const ERROR_MESSAGE =  'Строка не является корректной математической операцией';
+const LINE_SEPARATOR = '\r\n';
+const CALCULATOR = new Calculator();
+
+let cancelPressed = false;
+let response;
+
+while (!cancelPressed) {
+    let rawStringValue = prompt(`${response ? response : ''} ${LINE_SEPARATOR}Введите математическое выражение.`);
+
+    if (!rawStringValue) {
+        cancelPressed = true;
+        continue;
+    }
+
+    try {
+        response = CALCULATOR.evaluateExpression(rawStringValue);
+    } catch (error) {
+        response = ERROR_MESSAGE;
+    }
+}
